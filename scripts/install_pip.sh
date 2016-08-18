3d32b811c792b513d1c6b80fd78ef542e323ddb0
Joel Merrick <joel.merrick@datacentred.co.uk>
2016-08-18 10:33:33 +0100

    Added some SSL requirements

--- scripts/install_pip.sh
+++ scripts/install_pip.sh
@@ -4,8 +4,13 @@
 sudo apt-get update
 
 # Install necessary packages
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y install python-pip python-dev ca-certificates language-pack-en
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install python-pip python-dev ca-certificates language-pack-en libffi-dev libcurl4-openssl-dev libssl-dev
+
+sudo pip install --upgrade --force pip
 
 # Install OpenStack cli tools
+sudo pip install -U cffi
+sudo pip install --upgrade 'urllib3[secure]'
 sudo pip install pytz 
+sudo pip install cryptography idna
 sudo pip install python-{openstack,keystone,nova,glance,cinder,neutron,ceilometer,heat}client 
